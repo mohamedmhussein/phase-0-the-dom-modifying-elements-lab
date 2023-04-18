ecf4d478b114eee86eec19a5793a8e4ba0c9ad89
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-04-17 21:37:02 -0400

    DOM lab done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById("main")
+main.remove()
+let newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.textContent = "Mohamed is the champion"
+document.body.append(newHeader);
